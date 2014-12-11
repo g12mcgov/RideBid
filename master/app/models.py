6a3994d458dec8a2b5e0de9fb54962bc074c81dc
Grant McGovern <grant@cobrain.com>
2014-12-11 22:11:39 +0000

    MongoDB overhaul

--- master/app/models.py
+++ master/app/models.py
@@ -1,20 +1,45 @@
 ## Define database models
 
+'''
 from django.db import models
 
 class Category(models.Model):
-	name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=128, unique=True)
 
-	## Provides us with a unicode representation of model instance
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
 class Page(models.Model):
-	category = models.ForeignKey(Category)
-	title = models.CharField(max_length=128)
-	url = models.URLField()
-	views = models.IntegerField(default=0)
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
 
-	## Provides us with a unicode representation of model instance
-	def __unicode__(self):
-		return self.title
+    def __unicode__(self):
+        return self.title
+'''
+
+from django.db import models
+from djangotoolbox.fields import ListField
+from djangotoolbox.fields import EmbeddedModelField
+
+class Post(models.Model):
+	'''Stores Post objects'''
+	# Store time
+	created_on = models.DateTimeField(auto_now_add=True, null=True) 
+	title = models.CharField()
+	text = models.TextField()
+	tags = ListField()
+	comments = ListField(EmbeddedModelField('Comment'))
+
+class Comment(models.Model):
+	'''Stores Comment objects, part of Posts'''
+	created_on = models.DateTimeField(auto_now_add=True)
+	author = EmbeddedModelField('Author')
+	text = models.TextField()
+
+class Author(models.Model):
+	name = models.CharField()
+	email = models.CharField()
+
+	
--- master/master/settings.py
+++ master/master/settings.py
@@ -21,15 +21,27 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': DATABASE_PATH,                      # Or path to database file if using sqlite3.
+        # SQL_Lite
+        
+        #'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        #'NAME': 'DATABASE_PATH',                      # Or path to database file if using sqlite3.
+        
+        # MySQL
+        
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'rideboard',
+        #'USER': 'root',
+        #'PASSWORD': 'nantucket',
+        #'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        #'PORT': '',                      # Set to empty string for default.
+        
+        # Django
+        'ENGINE' : 'django_mongodb_engine',
+        'NAME' : 'rideboard'
     }
 }
 
